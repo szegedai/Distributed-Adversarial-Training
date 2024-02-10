1f9d1a760270f409d3c2c75b8cdf65270c15dece
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2024-02-10 14:19:48 +0100

    Update imagenet_test.py

--- imagenet_test.py
+++ imagenet_test.py
@@ -1,8 +1,9 @@
 import torch
 import torchvision
-import time
+import csv
 from dataloader.worker import DistributedAdversarialDataLoader
-
+from nn_utils.training import train_classifier, LRSchedulerCallback, CLILoggerCallback, Callback
+from nn_utils.models.resnet_v1 import wide_resnet28v1x10
 
 class LinfPGDAttack:
     def __init__(self, model, loss_fn, eps, step_size, num_steps, random_start=True, bounds=(0.0, 1.0)):
@@ -36,18 +37,51 @@ class LinfPGDAttack:
             delta = (x + delta).clamp(*self.bounds) - x
         return x + delta
 
+class CSVLoggerCallback(Callback):
+    def __init__(self, save_file):
+        self.save_file = save_file
+
+    def on_training_begin(self, training_vars):
+        col_names = ['train_loss', 'train_acc']
+        if training_vars['do_val']:
+            if training_vars['do_adv_val']:
+                col_names += ['adv_val_loss', 'adv_val_acc']
+            col_names += ['std_val_loss', 'std_val_acc']
+        with open(self.save_file, 'w', newline='') as fp:
+            csv.writer(fp).writerow(col_names)
+
+    def on_epoch_end(self, training_vars):
+        with open(self.save_file, 'a', newline='') as fp:
+            csv.writer(fp).writerow(training_vars['metrics'].values())
+
+
+class ModelUploaderCallback(Callback):
+    def __init__(self, upload_frequency=1):
+        self.upload_frequency = upload_frequency
+        self._model = None
+        self._train_loader = None
+
+    def on_batch_end(self, training_vars):
+        if training_vars['batch_idx'] % self.upload_frequency == 0:
+            training_vars['train_loader'].update_model(training_vars['model'])
+
 
 def main():
-    data_path = './imagenet_data'
+    data_path = '/imagenet'
+    save_path = '.'
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     #device = torch.device('cpu')
 
     train_loader = DistributedAdversarialDataLoader(
-        merge_batches=1,
-        pin_memory_device=device
+        host="http://127.0.0.1:3000",
+        batch_scale=1,
+        pin_memory_device=device,
+        buffer_size=10,
+        num_workers=4
     )
 
-    net = torchvision.models.resnet50(num_classes=1000).to(device)
+    #net = torchvision.models.resnet50(num_classes=1000).to(device)
+    net = wide_resnet28v1x10(1000).to(device)
 
     train_transform = torchvision.transforms.Compose([
         torchvision.transforms.ToTensor(),
@@ -56,7 +90,6 @@ def main():
     ])
 
     train_loader.update_model(net)
-
     train_loader.update_attack(
         LinfPGDAttack,
         net,
@@ -66,7 +99,7 @@ def main():
         10
     )
 
-    train_loader.set_parameters(max_patiente=100000, queue_limit=5)
+    train_loader.set_parameters(max_patiente=100, queue_limit=10)
     train_loader.update_data(
         torchvision.datasets.ImageFolder, 
         [
@@ -77,47 +110,31 @@ def main():
         },
         [],
         {
-            'batch_size': 8, 
+            'batch_size': 64, 
             'shuffle': True, 
-            'num_workers': 4, 
+            'num_workers': 8, 
+            'prefetch_factor': 4,
             'multiprocessing_context': 'spawn', 
             'persistent_workers': True
         }
     )
 
+    train_loader.start()
+
     criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(net.parameters(), lr=0.05, momentum=0.9)
+    optimizer = torch.optim.Adam(net.parameters(), lr=0.05)
 
-    for epoch in range(2):
-        losses = []
-        accs = []
-        epoch_start_time = time.time()
-
-        for i, data in enumerate(train_loader, 0):
-            inputs, labels = data
-            inputs, labels = inputs.to(device), labels.to(device)
-
-            optimizer.zero_grad()
-
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            losses.append(loss.item())
-            accs.append((outputs.detach().argmax(1) == labels).sum().item() / labels.size(0))
-
-            if(i % 2 == 0):
-                train_loader.update_model(net)
-
-            print(f"[Epoch {epoch+1}, Batch {i+1} loss: {sum(losses) / len(losses):.4f}, accuracy: {100 * sum(accs) / len(accs):.2f}%")
-
-        epoch_end_time = time.time()
-        print(f"Epoch took {epoch_end_time - epoch_start_time:.4}s.")
-
+    train_classifier(
+        net, criterion, optimizer, train_loader, None, None,
+        callbacks=[
+            CLILoggerCallback(),
+            ModelUploaderCallback(2)
+            #CSVLoggerCallback(save_path + '/training3_logs.csv'),
+        ],
+        num_epochs=200
+    )
     train_loader.stop()
     print("Training finished.")
 
 if __name__ == '__main__':
     main()
-
